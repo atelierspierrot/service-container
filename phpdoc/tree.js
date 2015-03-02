98377783b78e51a42127bd4ed2a2a3c24595f479
piwi <me@e-piwi.fr>
2015-03-02 20:28:29 +0100

    add the documentation

---
+++ phpdoc/tree.js
@@ -0,0 +1,6 @@
+var tree = [["ServiceContainer","ServiceContainer.html","",
+                [["ServiceContainer","ServiceContainer\/ServiceContainer.html","",
+                [        ]],["ServiceContainerInterface","ServiceContainer\/ServiceContainerInterface.html","",
+                [        ]],["ServiceProviderInterface","ServiceContainer\/ServiceProviderInterface.html","",
+                [        ]]        ]]        ]
+
